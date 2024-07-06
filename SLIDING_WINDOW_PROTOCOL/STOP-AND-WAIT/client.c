23b476d9999ed18867295e1b07f247da0837bd48
Harikrishnan <haripdm9446718962@gmail.com>
2024-07-06 22:27:23 +0530

    sliding window

--- SLIDING_WINDOW_PROTOCOL/STOP-AND-WAIT/client.c
+++ SLIDING_WINDOW_PROTOCOL/STOP-AND-WAIT/client.c
@@ -1,72 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+int main()
 
-#define MAX_DATA_SIZE 1024
+{
+	char buf[100];
+	int k,nof;
+	socklen_t len;
+	int sock_desc;
+	struct sockaddr_in server;
+	sock_desc=socket(AF_INET, SOCK_STREAM,0);
 
-typedef struct {
-    char data[MAX_DATA_SIZE];
-} Packet;
+	if(sock_desc==-1)
+		printf("Error in Socket creation\n");
 
-typedef struct {
-    int frame_kind; // ACK:0, SEQ:1, FIN:2
-    int sq_no;
-    int ack;
-    Packet packet;
-} Frame;
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=INADDR_ANY;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
- 
-    int port = atoi(argv[1]);  // convert a string representation of an integer 
-    int sockfd;
-    struct sockaddr_in serverAddr;
-    char buffer[MAX_DATA_SIZE];
+	server.sin_port=3003;
+	k=connect(sock_desc, (struct sockaddr*) &server, sizeof(server));
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
-        exit(1);
-    }
-
-    memset(&serverAddr, '\0', sizeof(serverAddr));  //\0' has a value of 0
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    int frame_id = 0;
-    Frame frame_send;
-    Frame frame_recv;
-
-    while (1) {
-        frame_send.sq_no = frame_id;
-        frame_send.frame_kind = 1;
-        frame_send.ack = 0;
-
-        printf("Enter Data: ");
-        fgets(buffer, MAX_DATA_SIZE, stdin);  //stdin: Standard input stream. This stream is typically used for reading input from the user, such as keyboard input.
-        buffer[strcspn(buffer, "\n")] = '\0'; // Remove newline character    // In other words, it finds the index of the first occurrence of '\n' in buffer
-        strcpy(frame_send.packet.data, buffer);
-
-        sendto(sockfd, &frame_send, sizeof(Frame), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
-        printf("[+] Frame Sent\n");
-
-        recvfrom(sockfd, &frame_recv, sizeof(Frame), 0, NULL, NULL);
-
-        if (frame_recv.sq_no == 0 && frame_recv.ack == frame_id + 1) {
-            printf("[+] Ack Received\n");
-            frame_id++;
-        } else {
-            printf("[-] Ack Not Received\n");
-        }
-    }
-
-    close(sockfd);
-    return 0;
+	if(k==-1)
+		printf("Error in connecting to server\n");
+	printf("Socket Creation Successful\nConnected to Server\n");
+	printf("Enter the total number of frames:");
+	scanf("%d",&nof);
+	printf("\n");
+	sprintf(buf,"%d", nof);
+	k = send(sock_desc,buf,100,0);
+	for(int i=0;i<nof;i++)
+	{
+		if(i%2==0){
+			printf("Frame [0] Sent\n");
+			k=send(sock_desc,buf,100,0);
+			sleep(1);
+		}
+		else
+		{
+			printf("Frame [1] Sent\n");
+			k = send(sock_desc,buf,100,0);
+			sleep(1);
+		}
+		printf("Acknowledgement received from server for Frame [%d]\n",(i%2==0)?1:0);
+		k=read(sock_desc,buf,100);
+		printf("\n");
+	}
+	close(sock_desc);
+	return 0;
 }
--- SLIDING_WINDOW_PROTOCOL/STOP-AND-WAIT/server.c
+++ SLIDING_WINDOW_PROTOCOL/STOP-AND-WAIT/server.c
@@ -1,71 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+int main()
+{
+	char buf[100];
+	int k;
+	socklen_t len;
+	int sock_desc,temp_sock_desc;
+	struct sockaddr_in server,client;
+	
+	sock_desc=socket(AF_INET,SOCK_STREAM,0);
+	
+	if(sock_desc==-1)
+		printf("Error in Socket creation\n");
+	printf("Socket created successfully...\n");
 
-#define MAX_DATA_SIZE 1024
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=INADDR_ANY;
 
-typedef struct {
-    char data[MAX_DATA_SIZE];
-} Packet;
-
-typedef struct {
-    int frame_kind; // ACK:0, SEQ:1, FIN:2
-    int sq_no;
-    int ack;
-    Packet packet;
-} Frame;
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
-
-    int port = atoi(argv[1]);
-    int sockfd;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addr_size = sizeof(clientAddr);
-
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Socket creation failed");
-        exit(1);
-    }
-
-    memset(&serverAddr, '\0', sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Binding failed");
-        exit(1);
-    }
-
-    Frame frame_recv;
-    int frame_id = 0;
-
-    while (1) {
-        recvfrom(sockfd, &frame_recv, sizeof(Frame), 0, (struct sockaddr *)&clientAddr, &addr_size);
-        
-        if (frame_recv.frame_kind == 1 && frame_recv.sq_no == frame_id) {
-            printf("[+] Frame Received: %s\n", frame_recv.packet.data);
-            Frame ack_frame;
-            ack_frame.sq_no = 0;
-            ack_frame.frame_kind = 0;
-            ack_frame.ack = frame_recv.sq_no + 1;
-            sendto(sockfd, &ack_frame, sizeof(Frame), 0, (struct sockaddr *)&clientAddr, addr_size);
-            printf("[+] Ack Sent\n");
-            frame_id++;
-        } else {
-            printf("[-] Frame Not Received\n");
-        }
-    }
-
-    close(sockfd);
-    return 0;
+	server.sin_port=3003;
+	
+	
+	k=bind(sock_desc,(struct sockaddr*) &server,sizeof(server));
+	
+	if(k==-1)
+		printf("Error in binding\n");
+	printf("Socket binded successfully...\n");
+		
+	k=listen(sock_desc,5);
+	
+	if(k==-1)
+		printf("Error in listening\n");
+	printf("Server listening...\n");
+		
+	len=sizeof(client);
+	temp_sock_desc=accept(sock_desc,(struct sockaddr*) &client,&len);
+		
+	if(temp_sock_desc==-1)
+		printf("Error in temporary socket creation\n");
+	printf("Server accepted client\n\n");
+	k = recv(temp_sock_desc,buf,100,0);
+	int nof = atoi(buf);
+	for(int i=0;i<nof;i++)
+	{	if(i%2==0){
+			k=recv(temp_sock_desc,buf,100,0);
+			printf("Frame received from Client:Frame [0]\n");
+			sleep(1);
+		}
+		else
+		{
+			k=recv(temp_sock_desc,buf,100,0);
+			printf("Frame received from Client:Frame [1]\n");
+			sleep(1);
+		}
+		printf("Acknowledgement for Frame [%d]\n",(i%2==0)?1:0);
+		k=send(temp_sock_desc,buf,100,0);
+		printf("\n");
+	}
+	return 0;
 }
-    
